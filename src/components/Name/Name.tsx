f032f1d0e21783701a73c2869296f30d5d2c0fff
nataliaFrancisca <nathifrancisca@gmail.com>
2024-05-26 20:58:28 -0300

    refactor: changed the span text-size

--- src/components/Name/Name.tsx
+++ src/components/Name/Name.tsx
@@ -5,7 +5,7 @@ const Name = (props: {value: string, remove: TCallBackWithProps}) => {
 
     return (
         <article className='w-full bg-white py-2.5 px-5 flex justify-between rounded-md border-solid border-black border-4'>
-            <span className='text-lg'>{props.value}</span>
+            <span className='text-xl'>{props.value}</span>
             <button className='bg-transparent hover:scale-125 transition-transform' onClick={() => props.remove(props.value)}>
                 <img src={imgUrl} />
             </button>
