544add57ce6bbcbe4eb76bc54d52e62e33176c03
nataliaFrancisca <nathifrancisca@gmail.com>
2024-05-26 20:57:44 -0300

    feat: add a gradient for the button sort and edit button

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,18 +7,21 @@ export default {
         default: ['Josefin Sans', 'sans-serif'],
         title: ['Faster One', 'system-u']
       },
+
       backgroundImage: {
         'custom-gradient': 'linear-gradient(180deg, var(--tw-gradient-stops))',
+        'button-gradient': 'linear-gradient(360deg, var(--tw-gradient-stops))',
       },
+      
+      backgroundColor: {
+        'button-transparent': 'rgba(255,255,255,25%)'
+      },
+
       gradientColorStops: {
         'gradient-yellow': '#FCB045',
         'gradient-purple': '#833AB4',
         'gradient-red': '#FD1D1D'
       },
-
-      colors: {
-        gradient: 'linear-gradient(180deg, rgba(252,176,69,1) 0%, rgba(253,29,29,1) 50%, rgba(131,58,180,1) 100%)'
-      }
     },
   },
   plugins: [],
