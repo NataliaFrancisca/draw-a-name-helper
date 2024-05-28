a5d346a208d1834cdfba407da3c7b96717d30176
nataliaFrancisca <nathifrancisca@gmail.com>
2024-05-27 21:08:21 -0300

    feat: created a list component for the values that were sorted

---
+++ src/components/NameList/SortedList.tsx
@@ -0,0 +1,18 @@
+const SortedList = (props: {names: Array<string>}) => {
+
+    return(
+       <section className="flex flex-col bg-button-transparent p-6 rounded-md">
+            <label className="font-bold text-xl mb-2">Sorted Values:</label>
+
+            <ul className="list-decimal list-inside" role="list">
+                {props.names.map((value, key) => ( 
+                    <li className="marker:font-bold uppercase text-lg" key={key}>
+                        {value}
+                    </li>
+                ))}
+            </ul>
+       </section>
+    )
+}
+
+export default SortedList;
